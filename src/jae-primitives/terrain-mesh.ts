2d6eb782973b54e718da776d57f52befe0f328f0
Simon Ringeisen <simon.ringeisen@gmail.com>
2021-08-04 23:47:43 +0200

    Refactoring and improving terrain generation

--- src/jae-primitives/terrain-mesh.ts
+++ src/jae-primitives/terrain-mesh.ts
@@ -3,119 +3,151 @@ import { vec3 } from 'gl-matrix';
 import Mesh from './mesh.js';
 import Perlin from '../util/perlin.js';
 
+interface TerrainConfig {
+  scale: number;
+  octaves: number;
+  persistance: number;
+  lacunarity: number;
+  maxHeight: number;
+}
+
 export default class TerrainMesh extends Mesh {
-  private seed: number;
+  private perlinNoise: Perlin;
 
-  constructor(width: number, depth: number, maxHeight: number, size: number) {
-    super();
+  private width: number;
 
-    this.seed = Math.random();
+  private depth: number;
 
-    const perlinNoise = new Perlin();
+  private size: number;
 
-    const scale = 1;
-    const octaves = 4;
-    const persistance = 0.5;
-    const lacunarity = 2;
+  terrainConfig: TerrainConfig;
 
-    this.vertices = TerrainMesh.getVertices(
-      width,
-      depth,
-      maxHeight,
-      size,
-      (x, y) =>
-        perlinNoise.getLayeredNoise(
-          x,
-          y,
-          scale,
-          octaves,
-          persistance,
-          lacunarity
-        )
-    );
-
-    for (let j = 0; j < width * depth; j += 1) {
-      const c = this.vertices[3 * j + 2] / (maxHeight * 2 * Math.PI) + 0.5;
-      this.colors = this.colors.concat(c, c, c, 1.0);
-    }
-
-    for (let j = 0; j < depth - 1; j += 1) {
-      for (let i = 0; i < width - 1; i += 1) {
-        this.indices = this.indices.concat(
-          j * width + i,
-          j * width + i + 1,
-          (j + 1) * width + i + 1,
-          j * width + i,
-          (j + 1) * width + i + 1,
-          (j + 1) * width + i
-        );
-      }
-    }
-
-    this.vertexNormals = this.getNormals(width, depth);
-  }
-
-  static getVertices(
+  constructor(
     width: number,
     depth: number,
-    maxHeight: number,
     size: number,
-    zValue: (x: number, y: number) => number
+    terrainConfig: TerrainConfig
   ) {
-    const vertices: number[] = new Array(width * depth * 3);
+    super();
 
-    for (let i = 0; i < width; i += 1) {
-      for (let j = 0; j < depth; j += 1) {
-        const x = (i / width - 0.5) * size;
-        const y = (j / depth - 0.5) * size;
-        const z = zValue(x, y) * maxHeight;
-        vertices[3 * (width * j + i)] = x;
-        vertices[3 * (width * j + i) + 1] = y;
-        vertices[3 * (width * j + i) + 2] = z;
+    this.width = width;
+    this.depth = depth;
+    this.size = size;
+    this.terrainConfig = terrainConfig;
+
+    this.perlinNoise = new Perlin();
+
+    this.generateMesh();
+  }
+
+  public changeNoise() {
+    this.perlinNoise = new Perlin();
+    this.generateMesh();
+  }
+
+  public generateMesh() {
+    this.indices = this.getIndices();
+    this.vertices = this.getVertices();
+    this.colors = this.getColors();
+    this.vertexNormals = this.getNormals();
+  }
+
+  getVertices() {
+    const vertices: number[] = new Array(this.width * this.depth * 3);
+
+    const zValue: (x: number, y: number) => number = (x, y) =>
+      this.perlinNoise.getLayeredNoise(
+        x,
+        y,
+        this.terrainConfig.scale,
+        this.terrainConfig.octaves,
+        this.terrainConfig.persistance,
+        this.terrainConfig.lacunarity
+      );
+
+    for (let i = 0; i < this.width; i += 1) {
+      for (let j = 0; j < this.depth; j += 1) {
+        const x = (i / this.width - 0.5) * this.size;
+        const y = (j / this.depth - 0.5) * this.size;
+        const z = zValue(x, y) * this.terrainConfig.maxHeight;
+        vertices[3 * (this.width * j + i)] = x;
+        vertices[3 * (this.width * j + i) + 1] = y;
+        vertices[3 * (this.width * j + i) + 2] = z;
       }
     }
     return vertices;
   }
 
-  getNormals(width: number, depth: number) {
+  getIndices() {
+    const indices: number[] = new Array(this.width * this.depth * 6);
+    let idx: number = 0;
+    for (let j = 0; j < this.depth - 1; j += 1) {
+      for (let i = 0; i < this.width - 1; i += 1) {
+        indices[idx++] = j * this.width + i;
+        indices[idx++] = j * this.width + i + 1;
+        indices[idx++] = (j + 1) * this.width + i + 1;
+        indices[idx++] = j * this.width + i;
+        indices[idx++] = (j + 1) * this.width + i + 1;
+        indices[idx++] = (j + 1) * this.width + i;
+      }
+    }
+    return indices;
+  }
+
+  getColors() {
+    const colors: number[] = new Array(this.width * this.depth * 4);
+    for (let j = 0; j < this.width * this.depth; j += 1) {
+      const c =
+        this.vertices[3 * j + 2] /
+          (this.terrainConfig.maxHeight * 2 * Math.PI) +
+        0.5;
+      colors[j * 4] = c;
+      colors[j * 4 + 1] = c;
+      colors[j * 4 + 2] = c;
+      colors[j * 4 + 3] = 1.0;
+    }
+    return colors;
+  }
+
+  getNormals() {
     const normals: number[] = new Array(this.vertices.length);
 
-    for (let i = 0; i < width; i += 1) {
-      for (let j = 0; j < depth; j += 1) {
+    for (let i = 0; i < this.width; i += 1) {
+      for (let j = 0; j < this.depth; j += 1) {
         const deltaX: vec3 = vec3.create();
         const deltaY: vec3 = vec3.create();
 
         const startX = Math.max(1, i - 1);
-        const endX = Math.min(i + 1, width - 2);
+        const endX = Math.min(i + 1, this.width - 2);
 
         const startY = Math.max(1, j - 1);
-        const endY = Math.min(j + 1, depth - 2);
+        const endY = Math.min(j + 1, this.depth - 2);
 
         vec3.sub(
           deltaX,
           [
-            this.vertices[(j * width + endX) * 3],
-            this.vertices[(j * width + endX) * 3 + 1],
-            this.vertices[(j * width + endX) * 3 + 2],
+            this.vertices[(j * this.width + endX) * 3],
+            this.vertices[(j * this.width + endX) * 3 + 1],
+            this.vertices[(j * this.width + endX) * 3 + 2],
           ],
           [
-            this.vertices[(j * width + startX) * 3],
-            this.vertices[(j * width + startX) * 3 + 1],
-            this.vertices[(j * width + startX) * 3 + 2],
+            this.vertices[(j * this.width + startX) * 3],
+            this.vertices[(j * this.width + startX) * 3 + 1],
+            this.vertices[(j * this.width + startX) * 3 + 2],
           ]
         );
 
         vec3.sub(
           deltaY,
           [
-            this.vertices[(endY * width + i) * 3],
-            this.vertices[(endY * width + i) * 3 + 1],
-            this.vertices[(endY * width + i) * 3 + 2],
+            this.vertices[(endY * this.width + i) * 3],
+            this.vertices[(endY * this.width + i) * 3 + 1],
+            this.vertices[(endY * this.width + i) * 3 + 2],
           ],
           [
-            this.vertices[(startY * width + i) * 3],
-            this.vertices[(startY * width + i) * 3 + 1],
-            this.vertices[(startY * width + i) * 3 + 2],
+            this.vertices[(startY * this.width + i) * 3],
+            this.vertices[(startY * this.width + i) * 3 + 1],
+            this.vertices[(startY * this.width + i) * 3 + 2],
           ]
         );
 
@@ -125,9 +157,9 @@ export default class TerrainMesh extends Mesh {
         vec3.cross(n, deltaX, deltaY);
 
         [
-          normals[(j * width + i) * 3],
-          normals[(j * width + i) * 3 + 1],
-          normals[(j * width + i) * 3 + 2],
+          normals[(j * this.width + i) * 3],
+          normals[(j * this.width + i) * 3 + 1],
+          normals[(j * this.width + i) * 3 + 2],
         ] = n;
       }
     }
